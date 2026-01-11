cbeeb9530eba4e607e380309dc0c794f82adda9e
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:41:06 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "data_protection_backup_instance_disks" {
+  description = "All data_protection_backup_instance_disk resources"
+  value       = azurerm_data_protection_backup_instance_disk.data_protection_backup_instance_disks
+}
+
